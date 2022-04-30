bd421adcfa3ee48be5592d5aba875c374d88cc26
Matheus Nicolini <cunhanicolini@hotmail.com>
2022-04-30 14:39:15 -0300

    Versionando back do projeto

---
+++ api/src/routes.js
@@ -0,0 +1,40 @@
+
+import { Router } from 'express';
+
+// import VariavelController from './app/controllers/VariavelController';
+// import QuestaoController from './app/controllers/QuestaoController';
+// import ResultadoController from './app/controllers/ResultadoController';
+// import SolucaoController from './app/controllers/SolucaoController';
+// import RegraController from './app/controllers/RegraController';
+// import SistemaController from './app/controllers/SistemaController';
+
+const routes = new Router();
+
+// routes.post('/variaveis', VariavelController.store);
+// routes.get('/variaveis', VariavelController.index);
+// routes.put('/variaveis/:id', VariavelController.update);
+// routes.delete('/variaveis/:ids', VariavelController.destroy);
+
+// routes.post('/questoes', QuestaoController.store);
+// routes.get('/questoes', QuestaoController.index);
+// routes.put('/questoes/:id', QuestaoController.update);
+// routes.delete('/questoes/:ids', QuestaoController.destroy);
+
+// routes.post('/resultados', ResultadoController.store);
+// routes.get('/resultados', ResultadoController.index);
+// routes.put('/resultados/:id', ResultadoController.update);
+// routes.delete('/resultados/:ids', ResultadoController.destroy);
+
+// routes.post('/solucoes', SolucaoController.store);
+// routes.get('/solucoes', SolucaoController.index);
+// routes.put('/solucoes/:id', SolucaoController.update);
+// routes.delete('/solucoes/:ids', SolucaoController.destroy);
+
+// routes.post('/regras', RegraController.store);
+// routes.get('/regras', RegraController.index);
+// routes.put('/regras/:id', RegraController.update);
+// routes.delete('/regras/:ids', RegraController.destroy);
+
+// routes.get('/gerar-resultado', SistemaController.gerarResultado);
+
+export default routes;
